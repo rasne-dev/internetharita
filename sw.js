e86c9f99baedb5948eae432edc2c55681788b96b
rasne-dev <ensarkaralii@gmail.com>
2026-09-23 20:47:28 +0300

    fix: moderator panelinde gizlenen pin ve iss tekliflerinin listeden kalkmasi saglandi (v1.1.13)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'iharita-v16';
+const CACHE = 'iharita-v17';
 const ASSETS = [
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
